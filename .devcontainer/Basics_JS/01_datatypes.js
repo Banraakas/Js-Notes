dc479197d46a432a7fc51bd41a75928b72509a8b
Gopi Singh <Firstimeronlygithub@gmail.com>
2024-06-16 17:22:51 +0000

    Different data types

---
+++ .devcontainer/Basics_JS/01_datatypes.js
@@ -0,0 +1,12 @@
+//alert(3+3)  this we are executing in node js env not browser env
+console.log(3+3);
+let namew = "Gopi"
+let age = 31
+let iscoder = true
+let village = null
+let ward;
+
+console.log(typeof namew )
+console.log(typeof iscoder )
+console.log(typeof village ) //Null is object
+console.log(typeof ward )// undefined is type
